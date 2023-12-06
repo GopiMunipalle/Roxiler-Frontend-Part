55c4636f2f5121a3e296138c9f05b381a1928fbf
unknown <munipallegopikrishna@gmail.com>
2023-12-06 14:51:29 +0530

    Frontend Part

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TransactionTable from "./components/TransactionTable";
+import Static from "./components/Static";
+import Barchart from "./components/Barchart";
+import Piechart from "./components/Piechart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TransactionTable />} />
+        <Route path="/static" element={<Static />} />
+        <Route path="/barchart" element={<Barchart />} />
+        <Route path="/piechart" element={<Piechart />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
